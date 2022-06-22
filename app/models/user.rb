8ea380df5017871bb61fb946f6e52dda1d238820
Ryota9 <ngm9dwf@gmail.com>
2022-06-22 21:17:42 +0900

    emailではなくusernameで認証するように変更。

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,34 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:username]
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
+
+
+  # nameを利用してログイン
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(['username = :value', { :value => username }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+
 end
