ba9f346509d20a682cba8d40e290aa0a513d7041
Ryota9 <ngm9dwf@gmail.com>
2022-07-07 02:06:00 +0900

    チャット機能の大元の作成
    jquery導入
    channelの作成
    broadcast_jobの作成　など

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,4 +8,7 @@
 #  updated_at :datetime         not null
 #
 class Message < ApplicationRecord
+    validates :content, presence: true
+    # createの後にコミットする { MessageBroadcastJobのperformを遅延実行 引数はself }
+    after_create_commit { MessageBroadcastJob.perform_later self }
 end
