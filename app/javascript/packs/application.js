0c432335cfbbbcfa19e2d93aee50a4f61fa66dc5
Ryota9 <ngm9dwf@gmail.com>
2022-07-10 19:08:46 +0900

    チャット欄に自動スクロール機能を実装。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,16 @@ require('jquery');
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+window.onload = function() {
+    // ↪︎ windowがロードされた時にアクションを実行するように設定
+      if (document.getElementById("messages")) {
+        // ↪︎ messagesのIDがある場合に処理を実行させる（これがないとチャット画面がなくても常にJavaScriptが動いてしまいます）
+        var scrollPosition = document.getElementById("messages").scrollTop;
+        // ↪︎ messages要素のスクロールされた時の最も高い場所を取得
+        var scrollHeight = document.getElementById("messages").scrollHeight;
+        // ↪︎ messages要素自体の最も高い場所を取得
+        document.getElementById("messages").scrollTop = scrollHeight;
+        // ↪︎ area要素のスクロールされた時の最も高い場所をarea要素自体の最も高い場所として指定してあげる
+      }
+    }
