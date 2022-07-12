08e19054a0696b7f700eacb7686fc8582966dcd6
Ryota9 <ngm9dwf@gmail.com>
2022-07-12 13:47:26 +0900

    全体的に見栄えを整えた。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
     before_action :authenticate_user!
     def index
         @tasks= current_user.tasks
-        @notifications = Notification.all
+        @notifications = Notification.all.order(id: :DESC).last(2)
+
+        if Room.find_by(id: current_user.id).nil?
+            @room = Room.create!(id: current_user.id)
+        else 
+            @room = Room.find_by(id: current_user.id)
+        end
+        @messages = @room.messages.last(3)
+
     end
 end
